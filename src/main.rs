0259ea2d166587c2fb9a418e10eda9d797629244
charlie <charlie@maddex.co>
2021-12-29 08:41:55 +0000

    feat: discover vulnerable files
    Signed-off-by: charlie <charlie@maddex.co>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::{App, Arg};
+use walkdir::WalkDir;
+use zip::ZipArchive;
+
+fn main() -> Result<()> {
+    println!("Discovering Log4Shell vulnerability [CVE-2021-44832]");
+
+    let matches = App::new("log4j-scan").version(env!("CARGO_PKG_VERSION")).setting(clap::AppSettings::ArgRequiredElseHelp).arg(Arg::with_name("PATH").required(true).help("Provide path to scan (e.g. C:\\)")).get_matches();
+    println!("Starting discovery on {}\n", matches.value_of("PATH").unwrap());
+
+    for entry in WalkDir::new(matches.value_of("PATH").unwrap()).follow_links(true).same_file_system(true).into_iter().filter_map(|e| e.ok()) {
+        if entry.file_name() == "Logger.class" && entry.path().to_str().unwrap_or("").contains("log4j") {
+            println!("Extracted vulnerable file found: {}", entry.path().display());
+        } else if entry.file_name().to_str().unwrap_or("").ends_with(".jar") {
+            if entry.file_name().to_str().unwrap_or("").contains("2.17.1") {
+            } else {
+                if let Ok(reader) = std::fs::File::open(entry.path()) {
+                    if let Ok(mut zip) = ZipArchive::new(reader) {
+                        for i in 0..zip.len() {
+                            if let Ok(file) = zip.by_index(i) {
+                                let name = file.name();
+                                if name.ends_with("Logger.class") && name.contains("log4j") {
+                                    println!("Vulnerable file found: {}", entry.path().display());
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("\nDiscovery Complete.");
+    Ok(())
 }
